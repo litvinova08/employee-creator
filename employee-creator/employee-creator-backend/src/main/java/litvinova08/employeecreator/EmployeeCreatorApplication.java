764655b6a26704cd3f3f193734532cab36ee7ac5
litvinova08 <ma.litvinova08@gmail.com>
2023-02-25 23:48:50 +1100

    added CorsMapping

--- employee-creator/employee-creator-backend/src/main/java/litvinova08/employeecreator/EmployeeCreatorApplication.java
+++ employee-creator/employee-creator-backend/src/main/java/litvinova08/employeecreator/EmployeeCreatorApplication.java
@@ -1,13 +1,29 @@
 package litvinova08.employeecreator;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EmployeeCreatorApplication {
+	
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeCreatorApplication.class, args);
 	}
+	
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+            }
+        };
+    }
 
 }
--- employee-creator/employee-creator-backend/src/main/java/litvinova08/employeecreator/employee/EmployeeController.java
+++ employee-creator/employee-creator-backend/src/main/java/litvinova08/employeecreator/employee/EmployeeController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/employee")
 @CrossOrigin(origins = "http://localhost:5173")
+@Validated
 
 public class EmployeeController {
 
@@ -28,14 +30,14 @@ public class EmployeeController {
 	EmployeeService service;
 	
 	//add a new employee to a database
-	@PostMapping("/employee")
+	@PostMapping()
 	public ResponseEntity<Employee> create(@Valid @RequestBody EmployeeCreateDTO data) {
 		Employee newEmployee = this.service.create(data);
 		return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
 	}
 	
 	//get all employees from a database
-	@GetMapping("/employee")
+	@GetMapping()
 	public ResponseEntity<List<Employee>> getAll() {	
 		List<Employee> allEmployees = this.service.getAll();
 		return new ResponseEntity<>(allEmployees, HttpStatus.OK);
@@ -43,7 +45,7 @@ public class EmployeeController {
 
 
 	//get an employee by id
-	@GetMapping("/employee/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<Employee> getById(@PathVariable Long id) {
 		Optional<Employee> maybeEmployee = this.service.getById(id);
 		
@@ -55,7 +57,7 @@ public class EmployeeController {
 	}
 	
 	//delete an employee by id
-	@DeleteMapping("/employee/{id}")
+	@DeleteMapping("/{id}")
 	public ResponseEntity<Employee> deleteById(@PathVariable Long id) {
 		boolean isDeleted = this.service.deleteById(id);
 		
